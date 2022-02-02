e5e9de92a819036e20fc51c9df8729dedec647dd
Dmitry Kalinin <kalinin.mitko@gmail.com>
2022-02-03 01:31:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ setup(
     author="Dmitry Kalinin",
     packages=["threadpools"],
     url="https://github.com/null-none/threadpools",
-    install_requires=[
+    keywords=[
         "thread",
         "pool",
         "thread pool"
         "thread pools"
     ],
+    install_requires=[],
 )
